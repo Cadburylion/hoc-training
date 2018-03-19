7ff00889e1a9df0e2612f14839bb4a5e9a7f2cdf
Matthew Parker <cadburylion@outlook.com>
2018-03-19 08:06:48 -0700

    Added boilerplate

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>eggs</li>
+        <li>spinach</li>
+        <li>secrets</li>
+      </ul>
+    </div>
+  );
+};
